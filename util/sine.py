f260b5ef9138345b993c58feb665de77b5da6f0b
Rybakov Vladimir <v.rybakov@corp.mail.ru>
2018-08-30 14:45:01 +0300

    This script print to screen sin(i)  where i == 0 - 2*PI

---
+++ util/sine.py
@@ -0,0 +1,16 @@
+# print sine to screen
+# require python 2.7
+#
+
+import math
+
+def get_sine():
+  n = 16
+  for i in range(0, n):
+    a = math.sin(2*math.pi * i / n)
+    d = (a + 1)*0x80
+    if d >= 0xFF:
+      d = 0xFF
+    print("0x%02X // %.4f" % (d, a))
+
+get_sine()
